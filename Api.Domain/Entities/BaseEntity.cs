107385e168a5cc719ab7d963d8f72c6a143a9d65
Junior <junior@MacBook-Pro-de-Jose.local>
2021-08-06 10:06:11 -0300

    06/08/2021 - first up

---
+++ Api.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? dateTime { get; set; }
+        private DateTime? _createAt;
+
+        public DateTime? CreatedAt{
+
+            get { return _createAt; }
+
+            set { _createAt = (value == null ? DateTime.UtcNow : value); }
+        }
+    }
+}
