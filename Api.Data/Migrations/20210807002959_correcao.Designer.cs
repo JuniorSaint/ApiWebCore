bda30d9eb12c039e68cf1539a7c27fb62d2d1c6e
Junior <junior@MacBook-Pro-de-Jose.local>
2021-08-06 21:42:54 -0300

    06/08/2021 - http-Delete

--- Api.Data/Migrations/20210807002959_correcao.Designer.cs
+++ Api.Data/Migrations/20210807002959_correcao.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Api.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace Api.Data.Migrations
 {
     [DbContext(typeof(MyContext))]
-    partial class MyContextModelSnapshot : ModelSnapshot
+    [Migration("20210807002959_correcao")]
+    partial class correcao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Api.Data/Migrations/20210807002959_correcao.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Data.Migrations
+{
+    public partial class correcao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "dateTime",
+                table: "User",
+                newName: "UpdateAt");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdateAt",
+                table: "User",
+                newName: "dateTime");
+        }
+    }
+}
